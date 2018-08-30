a219e861c734df718dc5b21e98277beed3309f6b
ionware <ionwarez@gmail.com>
2018-08-30 16:13:14 +0100

    Intial commit

---
+++ 503.blade.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon shortcut-icon" href="{{ asset('themes/ffande/assets/images/FF_E2.png') }}">
+    <title>Site Under Maintenance</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,900" rel="stylesheet">
+    <style>
+        body {
+            font-family: Montserrat, sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="row h-100 justify-content-center align-items-center">
+            <div class="col-xs-12 col-sm-10 col-md-8 text-center">
+                <img src="{{ asset('themes/ffande/assets/images/ffelogotrans.png') }}" alt="Logo" width="300" height="120">
+                <h2>
+                    Site Under Maintenance
+                </h2>
+                <p>
+                    We are working on some improvement. We'd be right back!
+                </p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
